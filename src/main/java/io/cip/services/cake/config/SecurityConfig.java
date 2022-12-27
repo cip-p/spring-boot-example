6fd16a069c057647aee34649515d21239946a73d
cip <ciprian.pataca@gmail.com>
2022-12-27 14:39:21 +0000

    spring boot 3.0.1, spring cloud 2022.0.0

--- src/main/java/io/cip/services/cake/config/SecurityConfig.java
+++ src/main/java/io/cip/services/cake/config/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .authorizeRequests()
+                .authorizeHttpRequests()
                 .anyRequest()
                 .authenticated()
                 .and()
